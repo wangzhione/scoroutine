655e0b40a15d1a29a3f71bf76d96e24f954d927b
wangzhi <wangzhione@163.com>
2016-10-22 19:22:26 +0800

    跨平台的协程库初版

---
+++ Makefile
@@ -0,0 +1,29 @@
+﻿# 全局替换变量
+CC = gcc 
+CFLAGS = -g -Wall -O2 
+RUNE = $(CC) $(CFLAGS) -o $@ $^
+
+# 声明路径变量
+SRC_PATH := ./coroutine
+TAR_PATH := ./Debug
+
+# 构建伪命令
+.PHONY:all clean cleanall
+
+# 第一个标签, 是make的开始
+all:$(TAR_PATH)/main.out
+
+$(TAR_PATH)/main.out:main.o scoroutine.o
+	$(CC) $(CFLAGS) -o $@ $(addprefix $(TAR_PATH)/, $^ )
+
+$(TAR_PATH):
+	mkdir $@
+	
+%.o:$(SRC_PATH)/%.c | $(TAR_PATH)
+	$(CC) $(CFLAGS) -c -o $(TAR_PATH)/$@ $<
+	
+# 清除操作
+clean:
+	-rm -rf $(TAR_PATH)/*.i $(TAR_PATH)/*.s $(TAR_PATH)/*.o $(TAR_PATH)/*~
+cleanall:clean
+	-rm -rf $(TAR_PATH)
---
+++ coroutine/main.c
@@ -0,0 +1,54 @@
+﻿#include <stdio.h>
+#include "scoroutine.h"
+
+#define _INT_TEST	(5)
+
+struct args {
+	int n;
+};
+
+static void _foo(void * sco, void * arg) {
+	struct args * as = arg;
+	int start = as->n;
+	int i = -1;
+
+	while (++i < _INT_TEST) {
+		printf("coroutine %d : %d.\n", sco_running(sco), start + i);
+		sco_yield(sco);
+	}
+}
+
+static void _test(void * sco) {
+	struct args argo = { 000 };
+	struct args argt = { 100 };
+
+	int coo = sco_create(sco, _foo, &argo);
+	int cot = sco_create(sco, _foo, &argt);
+
+	puts("********************_test start********************");
+	while (sco_status(sco, coo) && sco_status(sco, cot)) {
+		sco_resume(sco, coo);
+		sco_resume(sco, cot);
+	}
+	puts("********************_test e n d********************");
+}
+
+/*
+ * 主逻辑, 主要测试多个协程之间的纠缠
+ */
+int main(void) {
+	void * sco = sco_open();
+	
+	puts("--------------------突然想起了什么,--------------------\n");
+	_test(sco);
+
+	// 再来测试一下, 纤程切换问题
+	struct args arg = { 222 };
+	int co = sco_create(sco, _foo, &arg);
+	for (int i = -1; i < _INT_TEST; ++i)
+		sco_resume(sco, co);
+
+	puts("\n--------------------笑了笑, 我自己.--------------------");
+	sco_close(sco);
+	return 0;
+}
